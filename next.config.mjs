5cf7c1fe00ced3ce4531d51f20e934c8e9a3608a
Jiang <tulubus9420@gmail.com>
2026-07-15 05:39:04 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,45 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  img-src 'self' blob: data: https://*.basemaps.cartocdn.com;
+  connect-src 'self' https://nominatim.openstreetmap.org;
+  frame-ancestors 'none';
+  require-trusted-types-for 'script';
+`.replace(/\s{2,}/g, ' ').trim();
+
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'Cross-Origin-Opener-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: cspHeader,
+  }
+];
+
 const nextConfig = {
   swcMinify: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
   webpack: (config, { webpack }) => {
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(
